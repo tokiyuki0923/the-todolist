11f2397077b0a7adccf781aff277696e56c94dc3
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-22 20:42:52 +0900

    登録ボタンがクリックされた時と、Enterボタンが押された時にinputタグに書かれている内容をthタグとしてtableタグに追加する

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 "use strict";
 
+// それぞれ必要な情報を取得
+const form1 = document.getElementById("form1");
 const input = document.getElementById("input");
 const priority = document.getElementById("priority");
 const calendar = document.getElementById("calendar");
 const register = document.getElementById("register");
+const table = document.getElementById("table");
 
-console.log(input,priority,calendar,register)
+console.log(priority);
+// 登録ボタンがクリックされた時に発動する関数
+register.addEventListener("click",function (event){
+    event.preventDefault();
+    addText();
+});
+// Enterが押された時に発動する関数
+form1.addEventListener("submit",function(event){
+    event.preventDefault();
+    addText();
+});
+
+// inputタグに書かれているテキストを加えるというコールバック関数
+function addText(){
+    let text = input.value;
+    if(text){
+        const th = document.createElement("th")
+        th.innerText = text;
+        th.classList.add("toDoList");
+        table.appendChild(th);
+        input.value = "";
+    }
+}
+
+function addPriority(){
+}
+
